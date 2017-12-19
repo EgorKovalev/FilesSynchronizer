1d20e49636189a4dfb6e4beabe0e5a2c4bb8e90b
Yahor Kavaliou <yahor.kavaliou@solbeg.com>
2017-12-19 12:04:23 +0300

    get token for yandex service methods

---
+++ FileServiceApi/Controllers/yandexdiscController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Models;
+using FileServiceApi.Services;
+using Newtonsoft.Json;
+
+namespace FileServiceApi.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/yandexdisc")]
+    public class yandexdiscController : Controller
+    {
+        /// <summary>
+        /// GET api/yandexdisc/authlink
+        /// </summary>
+        /// <returns>link to login into the yandex disc service</returns>        
+        [HttpGet("authlink")]
+        public string GetAuthCode()
+        {
+            var service = new YandexService();
+            return service.Login();
+        }
+
+        /// <summary>
+        /// GET api/yandexdisc/token
+        /// </summary>
+        /// <param name="model">contains a code to get authorization token</param>
+        /// <returns>authorization token</returns>
+        [HttpPost("token")]
+        public TokenModel GetToken(AuthorizationModel model)
+        {
+            var service = new YandexService();
+            var json = service.GetToken(model).Result;
+
+            return JsonConvert.DeserializeObject<TokenModel>(json);
+        }
+    }
+}
---
+++ FileServiceApi/Services/YandexService.cs
@@ -0,0 +1,53 @@
+﻿using Models;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace FileServiceApi.Services
+{
+    public class YandexService
+    {
+        public YandexService()
+        { }
+
+        private string GetAuthRequestLink()
+        {
+            return YandexDiscAppClient.BaseUrl + "/authorize?response_type=code&client_id=" + YandexDiscAppClient.ClientId;
+        }
+
+        public string Login()
+        {
+            var link = GetAuthRequestLink();
+
+            return GetAuthRequestLink();
+        }
+
+        public async Task<string> GetToken(AuthorizationModel model)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(YandexDiscAppClient.BaseUrl);
+                var content = new FormUrlEncodedContent(new[]
+                {
+                new KeyValuePair<string, string>("code", model.Code),                
+                new KeyValuePair<string, string>("client_id", YandexDiscAppClient.ClientId),
+                new KeyValuePair<string, string>("client_secret", YandexDiscAppClient.ClientSecret),
+                new KeyValuePair<string, string>("grant_type", YandexDiscAppClient.GrandType)
+                });
+
+                var result = await client.PostAsync("/token", content);
+                return await result.Content.ReadAsStringAsync();
+            }
+        }
+
+        internal abstract class YandexDiscAppClient
+        {
+            public static string RedirectUrl { get; } = @"http://localhost:46278/swagger";
+            public static string ClientId { get; } = "80efe41b160b4dbe89dbba9b9f33e543";
+            public static string ClientSecret { get; } = "93fe81cc72344967a039d3969b2ee7d8";
+            public static string GrandType { get; } = "authorization_code";
+            public static string BaseUrl { get; } = @"https://oauth.yandex.ru";
+        }
+    }
+}
